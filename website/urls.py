bdfc0c81321d20c3c5077adb537a75ea7779a315
RileyT1D <rileyspencer08@gmail.com>
2025-09-08 13:37:41 +0100

    test

--- website/urls.py
+++ website/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name=""),
     #path('store', views.store, name="store"),
-    path('login', views.login, name="login")
+    path('login', views.login, name="login"),
+    path('register', views.register, name="register"),
 ]
--- website/views.py
+++ website/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
 
+from .forms import CreateUserForm, LoginForm
+
 # Create your views here.
 def home(request):
     return render(request, 'pages/index.html')
 
 def login(request):
     return render(request, 'pages/login.html')
+
+def register(request):
+    # make a form from forms.py
+    form = CreateUserForm()
+
+    if request.method == "POST":
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+    context = {'form' : form}
+
+    return render(request, 'pages/register.html', context=context)
